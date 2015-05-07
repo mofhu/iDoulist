f3782b3ea700afe4ad8f442956b2f426ba1f9a08
Frank Hu <humour114+git@gmail.com>
2015-05-07 22:46:02 +0800

    function0-input.py v1.0

--- input/function0-input.py
+++ input/function0-input.py
@@ -2,8 +2,33 @@
 # Author Frank Hu
 # iDoulist Function 0 - input
 
+import re
 import urllib2
 
-response = urllib2.urlopen("http://www.douban.com/doulist/38390646/")
+input_url = 'http://www.douban.com/doulist/38390646/'
+response = urllib2.urlopen(input_url)
 
-print re.findall('http://book.douban.com/subject/[0-9]*/', response.read())
+doulist_content = re.findall('http://book.douban.com/subject/[0-9]*/', response.read())
+
+print doulist_content
+
+'''
+ doulist_content = 
+['http://book.douban.com/subject/1139336/', 
+ 'http://book.douban.com/subject/1139336/', 
+ 'http://book.douban.com/subject/25724948/', 
+ 'http://book.douban.com/subject/25724948/']
+'''
+
+# remove duplicate
+
+i = 0
+while i <= len(doulist_content) - 1:
+    j = i + 1
+    while j <= len(doulist_content) - 1:
+        if doulist_content[i] == doulist_content[j]:
+            del doulist_content[j]
+        j += 1
+    i += 1
+
+print doulist_content
