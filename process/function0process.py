66cda6846d62c88e7b0ce9380e93b48f98ca2dfa
huhu8 <zhuhuhu118>
2015-05-08 10:20:54 +0800

    test0508

---
+++ process/function0process.py
@@ -0,0 +1,15 @@
+# iDoulist function 0 - process
+# Copy FROM Frank Hu 20150506
+ 
+# list is from input module
+ 
+list = ['http://book.douban.com/subject/1139336/',
+        'http://book.douban.com/subject/25724948/']
+ 
+print 'input list is :', list # for debug,难道在input阶段不输出用于debug吗
+ 
+new_list = list
+ 
+print 'new list is :', new_list # for debug
+ 
+# use new_list for output module
