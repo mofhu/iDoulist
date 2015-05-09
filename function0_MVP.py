e79e259d069c909fa18e8d1a2f1d7584c5dbd8da
Frank Hu <humour114+git@gmail.com>
2015-05-09 14:23:02 +0800

    finish function0-MVP and its test

--- function0_MVP.py
+++ function0_MVP.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 # Author Frank Hu
-# iDoulist Function 0 - loader
+# iDoulist Function 0 - MVP loader
 
 #import function0_UI
+import function0_UI
 import function0_input
 import function0_process
 import function0_output
 
 # get input from CLI
-
-input_url = 'http://www.douban.com/doulist/38390646/'
+input_url = function0_UI.doulist_input()
 
 # input 
+# with input url, std. input is a list, getting its content
 doulist_content = function0_input.doulist_url_to_list(input_url)
 
 # process
-output = function0_process.copy(doulist_content)
+# output is a list
+output_list = function0_process.copy(doulist_content)
 
 # output to file and CLI
-
-
+# output to markdown file and CLI
+function0_output.output_CLI(output_list)
+function0_output.output_file(output_list)
