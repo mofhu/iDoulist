552bca1e753f7fe79cf91dfb805f5c215b9b4eb4
ccQpein <ccq777@gmail.com>
2015-05-11 16:07:04 +0800

    change readme and script

--- output/function0_output.py
+++ output/function0_output.py
@@ -12,7 +12,7 @@ def output_CLI(output_doulist):
 	print 'iDoulist: your input doulist link contains: '
 	for i in output_doulist:
 		print i
-
+'''
 def output_file(output_doulist):
 	file_target = file('iDoulist.md', 'w')
 	file_target.write('iDoulist: your input doulist link contains: \n')
@@ -20,4 +20,18 @@ def output_file(output_doulist):
 		for ii in i:
 			file_target.write(ii + '\n')
 	print 'iDoulist: your input Doulist is exported as iDoulist.md'
-	file_target.close()
+	file_target.close()'''
+
+def output_file(output_doulist):
+	title = "iDoulist"
+
+	from jinja2 import Environment, FileSystemLoader
+	jinja_env = Environment(loader=FileSystemLoader('./'))
+	template = jinja_env.get_template('iDoulist.mdtemplate')
+
+	s = template.render(messages=output_doulist, title=title)
+
+	with open('{0}.md'.format(title), 'w') as f:
+		f.write(s.encode('utf-8'))
+
+	print 'iDoulist: your input Doulist is exported as {0}.md'.format(title)
