69dd68b14d55fb17983b8774f163dcf4e9b70c32
Frank Hu <humour114+git@gmail.com>
2015-05-15 23:50:00 +0800

    plain version: without jinja

---
+++ function0_MVP_plain.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Author Frank Hu
+# iDoulist Function 0 - MVP loader
+
+#import function0_UI
+from UI import function0_UI
+from input import function0_input
+from process import function0_process
+from output import function0_output_plain
+
+# get input from CLI
+input_url = function0_UI.doulist_input()
+
+# input 
+# with input url, std. input is a list, getting its content
+doulist_content = function0_input.doulist_url_to_list(input_url)
+
+# process
+# output is a list
+output_list = function0_process.copy(doulist_content)
+
+# output to file and CLI
+# output to markdown file and CLI
+function0_output_plain.output_CLI(output_list)
+function0_output_plain.output_file(output_list)
