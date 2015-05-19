b8413db403d53c2edf4adf4e83b497827dfbdd6e
Frank Hu <humour114+git@gmail.com>
2015-05-19 23:35:37 +0800

    function_MVP and its log

---
+++ function1_MVP_test_log.md
@@ -0,0 +1,22 @@
+```
+Franks-Mac-mini:iDoulist FrankHu$ python function1_MVP_plain.py
+Please input a Doulist url. e.g. http://www.douban.com/doulist/38390646/
+http://www.douban.com/doulist/37723990/
+Please input a Doulist url. e.g. http://www.douban.com/doulist/38390646/
+http://www.douban.com/doulist/38390646/
+Combined list:
+http://book.douban.com/subject/25724948/
+http://book.douban.com/subject/10770850/
+http://book.douban.com/subject/6709809/
+http://book.douban.com/subject/24749465/
+http://book.douban.com/subject/1139336/
+Common list::
+http://book.douban.com/subject/25724948/
+Doulist1 - Doulist 2:
+http://book.douban.com/subject/6709809/
+http://book.douban.com/subject/10770850/
+http://book.douban.com/subject/24749465/
+Doulist2 - Doulist 1:
+http://book.douban.com/subject/1139336/
+iDoulist: your input Doulist is exported as iDoulist.md
+```
---
+++ output/function1_output_plain.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Author Frank Hu
+# iDoulist Function 0 - output
+
+'''
+std test
+output_list = ['http://book.douban.com/subject/1139336/', 
+               'http://book.douban.com/subject/25724948/']
+'''
+
+def output_CLI(output_text, output_doulist):
+	print output_text + ':'
+	for i in output_doulist:
+		print i
+
+def output_file(output_text, output_doulist):
+	file_target = file('iDoulist.md', 'w')
+	file_target.write(output_text + '\n\n')
+	for i in output_doulist:
+		file_target.write(i + '\n\n')
+	print 'iDoulist: your input Doulist is exported as iDoulist.md'
+	file_target.close()
+
+'''
+def output_file(output_doulist):
+	title = "iDoulist"
+
+	from jinja2 import Environment, FileSystemLoader
+	jinja_env = Environment(loader=FileSystemLoader('./'))
+	template = jinja_env.get_template('iDoulist.mdtemplate')
+
+	s = template.render(messages=output_doulist, title=title)
+
+	with open('{0}.md'.format(title), 'w') as f:
+		f.write(s.encode('utf-8'))
+
+	print 'iDoulist: your input Doulist is exported as {0}.md'.format(title)'''
